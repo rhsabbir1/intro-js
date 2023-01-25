829ba4ff403014fb794db6aad64de9faa8f0fe37
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-25 22:52:28 +0600

    varibale name

---
+++ variable-name.js
@@ -0,0 +1,27 @@
+// 1.resurve key word not allow
+// like var true false
+var false = 52;
+var return = true ;
+
+
+// 2.varable name has to ber a one word 
+var friend name = 'atik';
+
+// 3. variable name cannot have a quotuation 
+var 'name' = 'Sabbir';
+
+// 4.variable name can not start a number 
+var 11noroom = 'Shakib';
+
+
+//------- how to use long name ---------
+var current home address = 'Jamlpur';
+var current_home_address = 'Jamlpur'; //one way
+var currentHomeAddress = 'Jamlpur'; // used js 
+var CurrentHomeAddress = 'Jamlpur'; // used js 
+
+
+// variable namwe is case sensitive 
+var studentName = 'Jamal';
+var StudentName = "Shadin";
+// they are not same 
