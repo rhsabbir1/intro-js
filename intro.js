4c3b2b6260d38c7fea576dd0c731babaa4b8f73a
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-25 22:21:59 +0600

    intro js

---
+++ intro.js
@@ -0,0 +1,13 @@
+// java script is a programming language . js is interprated language. 
+
+// today learn variables 
+// variable is a value that can change 
+
+// varable needs 5 items 
+// var [ this is key]
+// variable name [variable must be identifited with unique name ]
+// assingment operator [ = ]
+// value [ string numaric boolean ]
+// use - [ ;]
+
+var price = 12;
