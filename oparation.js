10c41b136d69eee5b4bb5b4b74fedeef24b85fd3
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 11:19:39 +0600

    simple math oparation

---
+++ oparation.js
@@ -0,0 +1,26 @@
+var eggPrice = 20;
+var onionPrice = 30;
+var totalPrice = eggPrice + onionPrice;
+
+var priceDifference = onionPrice - eggPrice;
+
+// console.log(eggPrice)
+// console.log(onionPrice)
+// console.log(eggPrice + onionPrice)
+// console.log(totalPrice)
+// console.log(priceDifference)
+
+
+// multiplaction 
+var orangPrice = 20;
+var quantity = 7;
+
+var totalPrice = orangPrice * quantity;
+// console.log(totalPrice)
+
+// Division 
+var money = 200;
+var player = 5;
+var eachplayer = money / player;
+console.log(eachplayer)
+
