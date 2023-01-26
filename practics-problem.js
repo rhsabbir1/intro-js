fb22a96c984a9a2609646216b7af888b23a3371e
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 16:13:14 +0600

    practics problems

---
+++ practics-problem.js
@@ -0,0 +1,41 @@
+// Practics Problem - 1
+var harrysMoney = 1000;
+
+var applePrice = 300;
+var orangePrice = 400;
+
+var totalCost = applePrice + orangePrice ;
+
+var harryCurrentMoney = harrysMoney - totalCost;
+console.log(harryCurrentMoney)
+
+// Practics Problem - 2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = mathematics + biology + bangla + chemistry + physics;
+
+var marksAvarage = totalMarks / 5;
+
+var marksAvarageDesimel = marksAvarage.toFixed(2)
+
+var marksAvarageNumber = parseFloat(marksAvarageDesimel)
+console.log(marksAvarageNumber)
+
+// Practics Problem - 3
+var firstLine = 'I am going to be';
+var secondLine = 'an awesome web developer';
+
+var fullLine = firstLine +' '+ secondLine;
+console.log(fullLine)
+
+// Practics Problem - 4
+
+var number = 119 ;
+var divisionNumber = 5;
+
+var remainder = number % divisionNumber;
+console.log(remainder)
