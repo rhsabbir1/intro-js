834ea0f42a41fef507f8e6e55bc6c32db69a776a
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 11:44:13 +0600

    short hand

---
+++ short-hand.js
@@ -0,0 +1,44 @@
+// variable diclearaton
+var tomatoPrice = 20;
+
+// value update 
+tomatoPrice = 35;
+
+// add smething to the previous value 
+var price1 = 30;
+var price2 = 10;
+var price3 = 10;
+var price4 = 20;
+
+
+// add some value to the existing value 
+
+price1 = price1 + 10;
+price2 += 10;
+
+// or
+price3 -= 5;
+
+// multiplication- short-hand 
+price4 *= 2;
+// console.log(price4)
+
+// division short-hand 
+price4 /= 2;
+
+// console.log(price1)
+// console.log(price2)
+// console.log(price3)
+// console.log(price4)
+
+var age = 15;
+age = age +1;
+age +=1;
+age++;
+console.log(age)
+
+var love = 100;
+love = love - 1;
+love -=1;
+love--;
+console.log(love)
