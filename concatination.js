7db1c458f1872ced316c49f840eafee5b66ccf68
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 12:04:55 +0600

    concatation and conversion added

---
+++ concatination.js
@@ -0,0 +1,11 @@
+var price1 = 50;
+var price2 = 20;
+var totalPrice = price1 + price2;
+// console.log(totalPrice)
+
+
+var firstName = "Arif";
+var lastName = "Ajad";
+
+var fullname = firstName +' '+ lastName;
+console.log(fullname)
---
+++ conversion.js
@@ -0,0 +1,23 @@
+var price1 = 41;
+var price2 = 29;
+var totalPrice = price1 + price2 ;
+// console.log(totalPrice)
+
+var price1 = "41";
+var price2 = 29;
+var totalPrice = price1 + price2 ;
+// console.log(totalPrice)
+
+var price1 = 41;
+var price2 = "29";
+var totalPrice = price1 + price2 ;
+// console.log(totalPrice)
+
+
+var price1 = "41";
+var price2 = "29";
+
+var price1Number = parseInt(price1)
+var price2Number = parseInt(price2)
+var totalPrice = price1Number + price2Number ;
+console.log(totalPrice)
